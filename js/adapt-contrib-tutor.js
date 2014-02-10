c0d932d54f2ae751048af6b5afe321cde8d41857
Daryl Hedley <darylhedley@hotmail.com>
2014-02-10 15:27:34 +0000

    Updated tutor to new style

--- js/adapt-contrib-tutor.js
+++ js/adapt-contrib-tutor.js
@@ -16,6 +16,7 @@ define(function(require) {
       this.render();
       this.listenTo(Adapt, 'remove', this.closeTutor, this);
       this.listenTo(Adapt, 'close', this.closeTutor, this);
+      this.listenTo(Adapt, 'device:resize', this.resetTutorSize);
     },
 
     events: {
@@ -26,16 +27,41 @@ define(function(require) {
     render: function () {
       var data = this.model.toJSON();
       var template = Handlebars.templates["tutor"];
-      $(this.el).html(template(data)).appendTo('#wrapper');
-      $('.tutor-inner').show();
-      $('.tutor-shadow').fadeIn('fast');
+      this.$el.html(template(data)).appendTo('#wrapper');
+      this.showTutor();
       return this;
+    },
+
+    resetTutorSize: function() {
+      $('.tutor').removeAttr('style');
+      this.resizeTutor(true);
+    },
+
+    resizeTutor: function(noAnimation) {
+      var windowHeight = $(window).height();
+      var tutorHeight = $('.tutor').height();
+      var animationSpeed = 400;
+      if (tutorHeight > windowHeight) {
+        $('.tutor').css({'height':'100%', 'top':0, 'overflow-y': 'scroll'})
+      } else {
+        if (noAnimation) {
+          var animationSpeed = 0;
+        }
+        $('.tutor').css({'margin-top': -(tutorHeight/2)-50, 'opacity': 0}).animate({'margin-top': -(tutorHeight/2), 'opacity':1}, animationSpeed);
+      }
+    },
+
+    showTutor: function() {
+      this.resizeTutor();
+      $('.tutor').show();
+      $('.tutor-shadow').fadeIn('fast');
     },
 
     closeTutor: function (event) {
       if (event && event.preventDefault) event.preventDefault();
-      $(this.$el).fadeOut('fast', _.bind(function() {
+      this.$el.fadeOut('fast', _.bind(function() {
         this.remove();
+        Adapt.trigger('tutor:closed');
       }, this));
     }
 
