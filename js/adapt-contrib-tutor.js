00965304cba98c2b7a6f3743a5ddc6f2f938796b
Guy Willis <guy.willis@kineo.com>
2019-01-17 13:44:57 +0000

    Converted tabs to spaces, indentation amends

--- js/adapt-contrib-tutor.js
+++ js/adapt-contrib-tutor.js
@@ -1,55 +1,56 @@
 define([
-    'coreJS/adapt'
+  'coreJS/adapt'
 ],function(Adapt) {
 
-    Adapt.on('questionView:showFeedback', function(view) {
+  Adapt.on('questionView:showFeedback', function(view) {
 
-        var alertObject = {
-            title: view.model.get("feedbackTitle"),
-            body: view.model.get("feedbackMessage")
-        };
+    var alertObject = {
+      title: view.model.get("feedbackTitle"),
+      body: view.model.get("feedbackMessage")
+    };
 
-        var attributes = {};
-        var classes = [];
+    var attributes = {};
+    var classes = [];
 
-        if (view.model.has('_isCorrect')) {
-            // Attach specific classes so that feedback can be styled.
-            if (view.model.get('_isCorrect')) {
-                classes.push('correct');
-            } else {
-                if (view.model.has('_isAtLeastOneCorrectSelection')) {
-                    // Partially correct feedback is an option.
-                    if (view.model.get('_isAtLeastOneCorrectSelection')) {
-                        classes.push('partially-correct');
-                    } else {
-                        classes.push('incorrect');
-                    }
-                } else {
-                    classes.push('incorrect');
-                }
-            }
+    if (view.model.has('_isCorrect')) {
+      // Attach specific classes so that feedback can be styled.
+      if (view.model.get('_isCorrect')) {
+        classes.push('correct');
+      } else {
+        if (view.model.has('_isAtLeastOneCorrectSelection')) {
+          // Partially correct feedback is an option.
+          if (view.model.get('_isAtLeastOneCorrectSelection')) {
+            classes.push('partially-correct');
+          } else {
+            classes.push('incorrect');
+          }
+        } else {
+          classes.push('incorrect');
         }
+      }
+    }
 
-        // Add the extension/component type which triggered this.
-        if (view.model.has('_component')) {
-            classes.push('component-' + view.model.get('_component'));
-        } else if (view.model.has('_extension')) {
-            classes.push('extension-' + view.model.get('_extension'));
-        }
+    // Add the extension/component type which triggered this.
+    if (view.model.has('_component')) {
+      classes.push('component-' + view.model.get('_component'));
+    } else if (view.model.has('_extension')) {
+      classes.push('extension-' + view.model.get('_extension'));
+    }
 
-        // Add the _id property as attribute.
-        attributes['data-adapt-id'] = view.model.get('_id');
+    // Add the _id property as attribute.
+    attributes['data-adapt-id'] = view.model.get('_id');
 
-        alertObject._classes = classes.join(' ');
-        alertObject._attributes = attributes;
+    alertObject._classes = classes.join(' ');
+    alertObject._attributes = attributes;
 
-        Adapt.once("notify:closed", function() {
-            Adapt.trigger("tutor:closed", view, alertObject);
-        });
-
-        Adapt.trigger('notify:popup', alertObject);
-
-        Adapt.trigger('tutor:opened', view, alertObject);
+    Adapt.once("notify:closed", function() {
+      Adapt.trigger("tutor:closed", view, alertObject);
     });
+
+    Adapt.trigger('notify:popup', alertObject);
+
+    Adapt.trigger('tutor:opened', view, alertObject);
+
+  });
 
 });
