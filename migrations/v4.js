7967cbdfbe9ef1742383c2f047f7368d66f28ea7
Brad Simpson <brad.simpson@kineo.com>
2025-01-28 15:44:46 -0700

    Add v4, globals

---
+++ migrations/v4.js
@@ -0,0 +1,36 @@
+import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
+
+describe('Tutor - v1.0.0 to v4.0.0', async () => {
+  let tutors, course, courseTutorGlobals;
+  whereFromPlugin('Tutor - from v1.0.0', { name: 'adapt-contrib-tutor', version: '<4.0.0' });
+  whereContent('Tutor - where tutor', async content => {
+    tutors = content.filter(({ _extension }) => _extension === 'tutor');
+    if (tutors) return true;
+  });
+  mutateContent('Tutor - add globals if missing', async (content) => {
+    course = content.find(({ _type }) => _type === 'course');
+    if (course?._globals?._extensions?._tutor) {
+      courseTutorGlobals = course._globals._extensions._tutor;
+      return true;
+    }
+    courseTutorGlobals = course._globals._extensions._tutor = {};
+    return true;
+  });
+  mutateContent('Tutor - add globals hideFeedback', async (content) => {
+    courseTutorGlobals.hideFeedback = 'Hide feedback';
+    return true;
+  });
+  checkContent('Tutor - check globals _tutor attribute', async content => {
+    if (courseTutorGlobals === undefined) {
+      throw new Error('Tutor - globals _tutor invalid');
+    }
+    return true;
+  });
+  checkContent('Tutor - check globals hideFeedback attribute', async content => {
+    if (courseTutorGlobals?.scrollAriaLabel === undefined) {
+      throw new Error('Tutor - globals hideFeedback invalid');
+    }
+    return true;
+  });
+  updatePlugin('Tutor - update to v4.0.0', { name: 'adapt-contrib-tutor', version: '4.0.0', framework: '>=5.18' });
+});
