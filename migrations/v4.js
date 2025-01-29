58a97058e6e176d7d1e293c6044d25e9ec5e6cde
Brad Simpson <brad.simpson@kineo.com>
2025-01-29 15:11:51 -0700

    Start updating components

--- migrations/v4.js
+++ migrations/v4.js
@@ -1,8 +1,9 @@
 import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
+import _ from 'lodash';
 
-describe('Tutor - v1.0.0 to v4.0.0', async () => {
+describe('Tutor - v2.1.0 to v4.0.0', async () => {
   let tutors, course, courseTutorGlobals;
-  whereFromPlugin('Tutor - from v1.0.0', { name: 'adapt-contrib-tutor', version: '<4.0.0' });
+  whereFromPlugin('Tutor - from v2.1.0', { name: 'adapt-contrib-tutor', version: '<4.0.0' });
   whereContent('Tutor - where tutor', async content => {
     tutors = content.filter(({ _extension }) => _extension === 'tutor');
     if (tutors) return true;
@@ -27,7 +28,7 @@ describe('Tutor - v1.0.0 to v4.0.0', async () => {
     return true;
   });
   checkContent('Tutor - check globals hideFeedback attribute', async content => {
-    if (courseTutorGlobals?.scrollAriaLabel === undefined) {
+    if (courseTutorGlobals?.hideFeedback === undefined) {
       throw new Error('Tutor - globals hideFeedback invalid');
     }
     return true;
@@ -35,13 +36,45 @@ describe('Tutor - v1.0.0 to v4.0.0', async () => {
   updatePlugin('Tutor - update to v4.0.0', { name: 'adapt-contrib-tutor', version: '4.0.0', framework: '>=5.18' });
 });
 
-describe('Tutor - v4.0.0 to v4.1.0', async () => {
-  let tutors, course, courseTutorGlobals;
-  whereFromPlugin('Tutor - from v4.1.0', { name: 'adapt-contrib-tutor', version: '<4.1.0' });
-  whereContent('Tutor - where tutor', async content => {
-    tutors = content.filter(({ _extension }) => _extension === 'tutor');
-    if (tutors) return true;
+// Update components
+describe('Tutor (components) - v2.1.0 to v4.0.0', async () => {
+  let components;
+  whereFromPlugin('Tutor (components) - from v2.1.0', { name: 'adapt-contrib-tutor', version: '<4.0.0' });
+  whereContent('Tutor - where component', async content => {
+    components = content.filter(({ _component }) => _component);
+    if (components) return true;
   });
-
-  updatePlugin('Tutor - update to v4.1.0', { name: 'adapt-contrib-tutor', version: '4.1.0', framework: '>=5.18' });
+  mutateContent('Tutor (components) - add _tutor if missing', async (content) => {
+    // console.log('\n COMPONENTS ' + components.length);
+    components.forEach(component => {
+      // console.log('\n' + component._id);
+      if (!_.has(component, '_tutor')) {
+        console.log('\n nope');
+        _.set(component, '_tutor', {});
+      }
+    });
+    return true;
+  });
+  checkContent('Tutor (components) - check for _tutor attribute', async content => {
+    let isValid = true;
+    components.forEach(component => {
+      if (!_.has(component, '_tutor')) isValid = false;
+    });
+    if (!isValid) {
+      throw new Error('Tutor (components) - component _tutor invalid');
+    }
+    return true;
+  });
+  updatePlugin('Tutor (components) - update to v4.0.0', { name: 'adapt-contrib-tutor', version: '4.0.0', framework: '>=5.18' });
 });
+
+// describe('Tutor - v4.0.0 to v4.1.0', async () => {
+//   let tutors, course, courseTutorGlobals;
+//   whereFromPlugin('Tutor - from v4.1.0', { name: 'adapt-contrib-tutor', version: '<4.1.0' });
+//   whereContent('Tutor - where tutor', async content => {
+//     tutors = content.filter(({ _extension }) => _extension === 'tutor');
+//     if (tutors) return true;
+//   });
+
+//   updatePlugin('Tutor - update to v4.1.0', { name: 'adapt-contrib-tutor', version: '4.1.0', framework: '>=5.18' });
+// });
