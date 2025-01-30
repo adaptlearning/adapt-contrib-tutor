fefd34a6e1047346404a0a698e59caffc288da96
Brad Simpson <brad.simpson@kineo.com>
2025-01-30 15:35:13 -0700

    Component properties, remove whereContent from v4.js

--- migrations/v4.js
+++ migrations/v4.js
@@ -4,14 +4,6 @@ import _ from 'lodash';
 describe('Tutor - v2.1.0 to v4.0.0', async () => {
   let course, courseTutorGlobals, components;
   whereFromPlugin('Tutor - from v2.1.0', { name: 'adapt-contrib-tutor', version: '<4.0.0' });
-  whereContent('Tutor - where ?', async (content) => {
-    // Check if Tutor is installed?? Configuration won't exist at this point.
-    // return true;
-
-    // Check if any components exist and set components variable
-    components = content.filter(({ _component }) => _component);
-    return components.length;
-  });
   mutateContent('Tutor - add globals if missing', async (content) => {
     course = content.find(({ _type }) => _type === 'course');
     if (!_.has(course, '_globals._extensions._tutor')) _.set(course, '_globals._extensions._tutor', {});
@@ -33,14 +25,51 @@ describe('Tutor - v2.1.0 to v4.0.0', async () => {
 
   // Update components
   mutateContent('Tutor - add _tutor to components if missing', async (content) => {
+    components = content.filter(({ _component }) => _component);
     components.forEach(component => {
       if (!_.has(component, '_tutor')) _.set(component, '_tutor', {});
     });
     return true;
   });
+  mutateContent('Tutor - add _tutor._type to components', async (content) => {
+    components.forEach(component => { _.set(component._tutor, '_type', 'notify'); });
+    return true;
+  });
+  mutateContent('Tutor - add _tutor._classes to components', async (content) => {
+    components.forEach(component => { _.set(component._tutor, '_classes', ''); });
+    return true;
+  });
+  mutateContent('Tutor - add _tutor._hasNotifyBottomButton to components', async (content) => {
+    components.forEach(component => { _.set(component._tutor, '_hasNotifyBottomButton', false); });
+    return true;
+  });
+  mutateContent('Tutor - add _tutor._button to components', async (content) => {
+    components.forEach(component => { _.set(component._tutor, '_button', {}); });
+    return true;
+  });
   checkContent('Tutor - check for _tutor component attribute', async content => {
     const isValid = components.every(({ _tutor }) => _tutor !== undefined);
-    if (!isValid) throw new Error('Tutor (components) - component _tutor invalid');
+    if (!isValid) throw new Error('Tutor - components _tutor invalid');
+    return true;
+  });
+  checkContent('Tutor - check for _tutor._type on components', async content => {
+    const isValid = components.every(({ _tutor }) => _tutor._type !== undefined);
+    if (!isValid) throw new Error('Tutor - component _tutor._type invalid');
+    return true;
+  });
+  checkContent('Tutor - check for _tutor._classes on components', async content => {
+    const isValid = components.every(({ _tutor }) => _tutor._classes !== undefined);
+    if (!isValid) throw new Error('Tutor - component _tutor._classes invalid');
+    return true;
+  });
+  checkContent('Tutor - check for _tutor._hasNotifyBottomButton on components', async content => {
+    const isValid = components.every(({ _tutor }) => _tutor._hasNotifyBottomButton !== undefined);
+    if (!isValid) throw new Error('Tutor - component _tutor._hasNotifyBottomButton invalid');
+    return true;
+  });
+  checkContent('Tutor - check for _tutor._button on components', async content => {
+    const isValid = components.every(({ _tutor }) => _tutor._button !== undefined);
+    if (!isValid) throw new Error('Tutor - component _tutor._button invalid');
     return true;
   });
   updatePlugin('Tutor - update to v4.0.0', { name: 'adapt-contrib-tutor', version: '4.0.0', framework: '>=5.18.0' });
