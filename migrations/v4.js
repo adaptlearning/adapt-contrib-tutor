cc1ad926db37abaee66f425a6f88caa8609ef0bc
Brad Simpson <brad.simpson@kineo.com>
2025-01-30 14:55:46 -0700

    Add _tutor to components

--- migrations/v4.js
+++ migrations/v4.js
@@ -2,11 +2,15 @@ import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, u
 import _ from 'lodash';
 
 describe('Tutor - v2.1.0 to v4.0.0', async () => {
-  let tutors, course, courseTutorGlobals, components;
+  let course, courseTutorGlobals, components;
   whereFromPlugin('Tutor - from v2.1.0', { name: 'adapt-contrib-tutor', version: '<4.0.0' });
-  whereContent('Tutor - where tutor', async content => {
-    tutors = content.filter(({ _extension }) => _extension === 'tutor');
-    return tutors.length;
+  whereContent('Tutor - where ?', async (content) => {
+    // Check if Tutor is installed?? Configuration won't exist at this point.
+    // return true;
+
+    // Check if any components exist and set components variable
+    components = content.filter(({ _component }) => _component);
+    return components.length;
   });
   mutateContent('Tutor - add globals if missing', async (content) => {
     course = content.find(({ _type }) => _type === 'course');
@@ -28,29 +32,15 @@ describe('Tutor - v2.1.0 to v4.0.0', async () => {
   });
 
   // Update components
-  whereContent('Tutor - where component', async content => {
-    components = content.filter(({ _component }) => _component);
-    return components.length;
-  });
   mutateContent('Tutor - add _tutor to components if missing', async (content) => {
-    console.log('\n COMPONENTS ' + components.length);
     components.forEach(component => {
-      console.log('\n' + component._id);
-      if (!_.has(component, '_tutor')) {
-        console.log('\n nope');
-        _.set(component, '_tutor', {});
-      }
+      if (!_.has(component, '_tutor')) _.set(component, '_tutor', {});
     });
     return true;
   });
   checkContent('Tutor - check for _tutor component attribute', async content => {
-    // let isValid = true;
-    // components.forEach(component => {
-    //   if (!_.has(component, '_tutor')) isValid = false;
-    // });
-    // if (!isValid) {
-    //   throw new Error('Tutor (components) - component _tutor invalid');
-    // }
+    const isValid = components.every(({ _tutor }) => _tutor !== undefined);
+    if (!isValid) throw new Error('Tutor (components) - component _tutor invalid');
     return true;
   });
   updatePlugin('Tutor - update to v4.0.0', { name: 'adapt-contrib-tutor', version: '4.0.0', framework: '>=5.18.0' });
