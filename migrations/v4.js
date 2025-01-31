fc71d9cb8bc61fd6ef84b82cba450c8d5964312a
Brad Simpson <brad.simpson@kineo.com>
2025-01-31 12:23:16 -0700

    4.1.0 course _tutor props

--- migrations/v4.js
+++ migrations/v4.js
@@ -1,11 +1,12 @@
 import { describe, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
 import _ from 'lodash';
 
+let courseTutorGlobals, components;
+
 describe('Tutor - v2.1.0 to v4.0.0', async () => {
-  let course, courseTutorGlobals, components;
   whereFromPlugin('Tutor - from v2.1.0', { name: 'adapt-contrib-tutor', version: '<4.0.0' });
   mutateContent('Tutor - add globals if missing', async (content) => {
-    course = content.find(({ _type }) => _type === 'course');
+    const course = content.find(({ _type }) => _type === 'course');
     if (!_.has(course, '_globals._extensions._tutor')) _.set(course, '_globals._extensions._tutor', {});
     courseTutorGlobals = course._globals._extensions._tutor;
     return true;
@@ -93,13 +94,84 @@ describe('Tutor - v2.1.0 to v4.0.0', async () => {
   updatePlugin('Tutor - update to v4.0.0', { name: 'adapt-contrib-tutor', version: '4.0.0', framework: '>=5.18.0' });
 });
 
-// describe('Tutor - v4.0.0 to v4.1.0', async () => {
-//   let tutors, course, courseTutorGlobals;
-//   whereFromPlugin('Tutor - from v4.1.0', { name: 'adapt-contrib-tutor', version: '<4.1.0' });
-//   whereContent('Tutor - where tutor', async content => {
-//     tutors = content.filter(({ _extension }) => _extension === 'tutor');
-//     if (tutors) return true;
-//   });
-
-//   updatePlugin('Tutor - update to v4.1.0', { name: 'adapt-contrib-tutor', version: '4.1.0', framework: '>=5.18.0' });
-// });
+describe('Tutor - v4.0.0 to v4.1.0', async () => {
+  whereFromPlugin('Tutor - from v4.0.0', { name: 'adapt-contrib-tutor', version: '<4.1.0' });
+  mutateContent('Tutor - add course _tutor', async (content) => {
+    const course = content.find(({ _type }) => _type === 'course');
+    if (!_.has(course, '_tutor')) { _.set(course, '_tutor', {}); }
+    return true;
+  });
+  mutateContent('Tutor - add course _tutor._type', async (content) => {
+    const course = content.find(({ _type }) => _type === 'course');
+    _.set(course._tutor, '_type', 'notify');
+    return true;
+  });
+  mutateContent('Tutor - add course _tutor._classes', async (content) => {
+    const course = content.find(({ _type }) => _type === 'course');
+    _.set(course._tutor, '_classes', '');
+    return true;
+  });
+  mutateContent('Tutor - add course _tutor._hasNotifyBottomButton', async (content) => {
+    const course = content.find(({ _type }) => _type === 'course');
+    _.set(course._tutor, '_hasNotifyBottomButton', false);
+    return true;
+  });
+  mutateContent('Tutor - add course _tutor._button', async (content) => {
+    const course = content.find(({ _type }) => _type === 'course');
+    _.set(course._tutor, '_button', {});
+    return true;
+  });
+  mutateContent('Tutor - add course _tutor._button', async (content) => {
+    const course = content.find(({ _type }) => _type === 'course');
+    _.set(course._tutor._button, 'text', '{{_globals._extensions._tutor.hideFeedback}}');
+    return true;
+  });
+  mutateContent('Tutor - add course _tutor.ariaLabel', async (content) => {
+    const course = content.find(({ _type }) => _type === 'course');
+    _.set(course._tutor._button, 'ariaLabel', '{{_globals._extensions._tutor.hideFeedback}}');
+    return true;
+  });
+  checkContent('Tutor - check for course _tutor', async content => {
+    const course = content.find(({ _type }) => _type === 'course');
+    const isValid = _.has(course, '_tutor');
+    if (!isValid) throw new Error('Tutor - course _tutor invalid');
+    return true;
+  });
+  checkContent('Tutor - check for course _tutor._type', async content => {
+    const course = content.find(({ _type }) => _type === 'course');
+    const isValid = _.has(course._tutor, '_type');
+    if (!isValid) throw new Error('Tutor - course _tutor._type invalid');
+    return true;
+  });
+  checkContent('Tutor - check for course _tutor._classes', async content => {
+    const course = content.find(({ _type }) => _type === 'course');
+    const isValid = _.has(course._tutor, '_classes');
+    if (!isValid) throw new Error('Tutor - course _tutor._classes invalid');
+    return true;
+  });
+  checkContent('Tutor - check for course _tutor._hasNotifyBottomButton', async content => {
+    const course = content.find(({ _type }) => _type === 'course');
+    const isValid = _.has(course._tutor, '_hasNotifyBottomButton');
+    if (!isValid) throw new Error('Tutor - course _tutor._hasNotifyBottomButton invalid');
+    return true;
+  });
+  checkContent('Tutor - check for course _tutor._button', async content => {
+    const course = content.find(({ _type }) => _type === 'course');
+    const isValid = _.has(course._tutor, '_button');
+    if (!isValid) throw new Error('Tutor - course _tutor._button invalid');
+    return true;
+  });
+  checkContent('Tutor - check for course _tutor._button.text', async content => {
+    const course = content.find(({ _type }) => _type === 'course');
+    const isValid = _.has(course._tutor._button, 'text');
+    if (!isValid) throw new Error('Tutor - course _tutor._button.text invalid');
+    return true;
+  });
+  checkContent('Tutor - check for course _tutor._button.ariaLabel', async content => {
+    const course = content.find(({ _type }) => _type === 'course');
+    const isValid = _.has(course._tutor._button, 'ariaLabel');
+    if (!isValid) throw new Error('Tutor - course _tutor._button.ariaLabel invalid');
+    return true;
+  });
+  updatePlugin('Tutor - update to v4.1.0', { name: 'adapt-contrib-tutor', version: '4.1.0', framework: '>=5.18.0' });
+});
